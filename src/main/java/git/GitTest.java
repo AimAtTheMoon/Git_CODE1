b7fe05e878b8d5ad18c9a34e66aa83f135a3cf9e
git-damo <2910867682@qq.com>
2022-03-01 20:29:25 +0800

    first commit

---
+++ src/main/java/git/GitTest.java
@@ -0,0 +1,11 @@
+package git;
+
+/**
+ * @author
+ * @create 2022-03-01-20:23
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
